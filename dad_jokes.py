73ed534ef95749d8fdf6dfa6e0d3462216ec526a
Otar Matchavariani <83397933+machavarian10to@users.noreply.github.com>
2022-02-06 14:00:06 +0400

    Update dad_jokes.py

--- dad_jokes.py
+++ dad_jokes.py
@@ -2,16 +2,16 @@ import requests
 import json
 import sqlite3
 
-conn = sqlite3.connect('dad-jokes.sqlite')
-cur = conn.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS jokes 
+connect = sqlite3.connect('dad-jokes.sqlite')
+curson = connect.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS jokes 
                (id INTEGER PRIMARY KEY AUTOINCREMENT, 
                type VARCHAR(40),
                setup VARCHAR(40),
                punchline VARCHAR(40)
                )''')
 
-userInput = int(input("შეიყვანე რიცხვი ხუმრობის ტიპის მიხედვით - 1=Programming, 2=Knock-knock, +3=General : "))
+userInput = int(input("Enter the number by joke type: 1 = Programming, 2 = Knock-knock, +3 = General : "))
 if userInput == 1:
     jokeType = 'programming'
 elif userInput == 2:
@@ -26,19 +26,19 @@ headers = {
     'x-rapidapi-host': "dad-jokes.p.rapidapi.com"
     }
 
-resp = requests.get(url, headers=headers)
-res = resp.json()
+response = requests.get(url, headers=headers)
+responseJson = response.json()
 with open('jokes.json', 'w') as f:
-    json.dump(res,f, indent=4)
-lst = []
-for i in res['body']:
+    json.dump(responseJson, f, indent=4)
+jokesList = []
+for i in responseJson['body']:
     type = i['type']
     setup = i['setup']
     punchline = i['punchline']
     data = (type,setup,punchline)
-    lst.append(data)
+    jokesList.append(data)
 
-conn.executemany('''INSERT INTO jokes (type,setup,punchline) VALUES(?,?,?)''', lst)
+conn.executemany('''INSERT INTO jokes (type,setup,punchline) VALUES(?,?,?)''', jokesList)
 conn.commit()
 conn.close()
 
